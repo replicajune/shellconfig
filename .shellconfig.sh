60369bad4e44fedd2d83f6043f8bcdcd87473bfa
replicajune <replicajune>
2019-03-30 02:35:08 +0100

    change: ps1 with : - git branch being printed when in a git repo - exit status - history number of upcoming commandline

--- .shellconfig.sh
+++ .shellconfig.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # PS1
-if [ $USER = "root" ]; then
-  PS1="\t [\[\e[0;31m\]\u\[\e[0m\]@\[\e[0;36m\]\h \[\e[0;34m\]\w\[\e[0m\]]# "
-else
-  PS1="\t [\[\e[0;32m\]\u\[\e[0m\]@\[\e[0;36m\]\h \[\e[0;34m\]\w\[\e[0m\]]$ "
-fi
+type __git_ps1 2> /dev/null | grep '()' &> /dev/null && _SCPS1GIT='$(__git_ps1 "(%s)")'
+echo $0 | grep 'bash' &> /dev/null && _PS1HISTNB='|\!\[\e[2;2m\]'
+PS1='\[\e[2;2m\]\t \[\e[0m\][\[\e[0;'$([ $USER = "root" ] && echo "31" || echo '32')'m\]\u\[\e[0m\]@\[\e[0;36m\]\h\[\e[0m\] \[\e[2;2m\]\w\[\e[0m\] \[\e[0;93m\]'$_SCPS1GIT'\[\e[0m\]] \[\e[2;2m\]$?'$_PS1HISTNB'\n> \[\e[0m\]'
+unset _SCPS1GIT _PS1HISTNB
 
 # tmux : disable this using "export TMUX=disable" before loading shellconfig
 if command -v tmux &> /dev/null && [ -z "$TMUX" ] && [ -z "$SUDO_USER" ]; then
