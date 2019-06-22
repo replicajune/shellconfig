c8d8bec5486f73c9b693460ba66283e9a2c814df
julien <julien>
2019-06-22 20:36:11 +0200

    fix: --
    - use return in fuctions instead of exit

--- .functions.sh
+++ .functions.sh
@@ -12,26 +12,28 @@ rambox_update () {
     yum)  PKG_TYPE="x86_64.rpm";
           PKG_INST_CMD="yum install -y";
       ;;
-    *) echo "sorry, not compatible" && exit 1;;
+    *) echo "sorry, not compatible" && return 1;;
   esac
 
   # get a list if available packages
   REPO='ramboxapp/community-edition'
   LATEST_RELEASES=$(
-    curl https://api.github.com/repos/${REPO}/releases/latest 2> /dev/null |
+    curl "https://api.github.com/repos/${REPO}/releases/latest" 2> /dev/null |
     jq ".assets |.[] | .browser_download_url" |
     tr -d '"'
   )
 
   for URL in ${LATEST_RELEASES}; do
     # get the right URL
-    if [[ "${URL##*-}" = "${PKG_TYPE}" ]]; then
+    if [ "${URL##*-}" = "${PKG_TYPE}" ]; then
       echo "downloading ..."
-      wget ${URL} --output-document="/tmp/${URL##*/}" --quiet
+      wget "${URL}" --output-document="/tmp/${URL##*/}" --quiet
       # install using defined package manager & manage a dpkg fail
       echo "installing, or upgrading ..."
+      # shellcheck disable=SC2086
       sudo ${PKG_INST_CMD} "/tmp/${URL##*/}" ||\
-        sudo ${PKG_INST_CMD_NOFAIL-'true'} || exit 2
+        # shellcheck disable=SC2086
+        sudo ${PKG_INST_CMD_NOFAIL-'true'} || return 2
       rm -f "/tmp/${URL##*/}"
       break
     fi
