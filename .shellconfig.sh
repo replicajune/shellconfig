59c1ac42f034e9a6ad2e23c15e96bb1af80a83ea
julien <julien>
2019-08-23 21:07:45 +0200

    change: print weather in a smaller form

--- .shellconfig.sh
+++ .shellconfig.sh
@@ -194,7 +194,7 @@ fi
 alias h="history |tail -20"
 alias vless="vim -M"
 alias datei="date --iso-8601=s"
-alias weather="curl wttr.in"
+alias weather="curl wttr.in/?0"
 
 # for personnal or private aliases (things with contexts and stuff)
 if [ -f "${HOME}/.aliases.private.sh" ]; then
