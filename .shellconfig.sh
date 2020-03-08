d33195ef54f668dbcff67aa1c52c48e05454872d
julien <julien>
2020-03-08 23:55:47 +0100

    change: align ls aliases with more expected behaviors

--- .shellconfig.sh
+++ .shellconfig.sh
@@ -45,11 +45,12 @@ fi
 
 # files managment
 alias l='ls -C --classify --group-directories-first'
-alias ll='ls -g --classify --group-directories-first --human-readable --no-group'
-alias la='ls -l --classify --group-directories-first --human-readable --no-group --almost-all'
-alias lll='ls -l --classify --group-directories-first --human-readable --context --inode  --author'
-alias lz='ls -g --classify --group-directories-first --human-readable --context --no-group'
-alias lt='ls -gt --classify --reverse --human-readable --no-group'
+alias ll='ls -l --classify --group-directories-first --human-readable'
+alias la='ls -l --classify --group-directories-first --human-readable --all'
+alias lll='ls -l --classify --group-directories-first --human-readable --context  --author'
+alias lla='ls -l --classify --group-directories-first --human-readable --context  --author --all'
+alias lz='ls -g --classify --group-directories-first --human-readable --context --no-group --all'
+alias lt='ls -gt --classify --reverse --human-readable --all --no-group'
 alias rm="rm -i"
 alias vd="diff --side-by-side --suppress-common-lines"
 alias send="rsync --archive --info=progress2 --human-readable --compress"
