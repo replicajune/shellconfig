4151eddb921ef50bf6367bfb1fa2b786cd16cc22
replicajune <replicajune@users.noreply.github.com>
2021-07-30 21:14:22 -0400

    :wastebasket: reverting bug test, cleaning

--- shells/shell.sh
+++ shells/shell.sh
@@ -3,8 +3,7 @@
 # files managment
 alias l='ls -C --group-directories-first'
 alias ll='ls -l --group-directories-first -h'
-alias la='ls -l --gro'
-up-directories-first -h -a'
+alias la='ls -l --group-directories-first -h -a'
 alias lt='ls -gt -r -h -a'
 
 alias lsm='mount | grep -E ^/dev'
