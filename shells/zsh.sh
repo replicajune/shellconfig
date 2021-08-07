397e61e10d932d1259766b9ad5ed3ea0b6a6de71
replicajune <replicajune@users.noreply.github.com>
2021-08-07 18:23:56 -0400

    :sparkles: "import" of bash like options

--- shells/zsh.sh
+++ shells/zsh.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env zsh
 
+setopt autocd
+setopt noclobber # don't override existing files
+setopt histverify
+setopt promptsubst
+
+bindkey -e # user emacs bindings in readline
+
+# moar autocompletion
+autoload -U compinit
+compinit
+
+# avoid end of lines addition when lines wne without newlines
 PROMPT_EOL_MARK=''
 
 bindkey -e
