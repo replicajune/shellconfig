7ac76609d3b2f0051298ea0b3ed968bc8269b30b
replicajune <replicajune@users.noreply.github.com>
2022-08-23 01:09:32 -0400

    📝 doc: move comments about disabled checks in shellcheck script

--- .ci/shellcheck.sh
+++ .ci/shellcheck.sh
@@ -12,3 +12,10 @@ while IFS= read -r -d '' SHFILE; do
 done < <(find . -name "*.sh" -type f -print0)
 
 if [ "${ERROR}" = '1' ]; then exit 1; fi
+
+# SC1090: https://github.com/koalaman/shellcheck/wiki/SC1090
+# - most tests in a CI job would miss the location of an expected
+#   file on target
+# SC2039,SC3037,SC3043,SC3003 : https://github.com/koalaman/shellcheck/wiki/SC2039
+# - using "local", echo flags, ect.. works on at least alpine and
+#   openwrt, if busybox is safe, this is enough for me.
