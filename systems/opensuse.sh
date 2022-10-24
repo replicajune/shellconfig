096c4a75621c6177827c18da30c8491c4d7bd979
replicajune <replicajune@users.noreply.github.com>
2022-10-24 11:26:51 -0400

    2022 42 (#93)
    * fix: tumbleweed can have a LOT of update. downloading packages firts, then see to apply update.
    
    * 👉 fix: "prefetch" need to happen on it's own. it's the apply part of upgrade I want an input on
    
    * ✨ add: set a token for sudo to avoid having sudo asked after flatpak update

--- systems/opensuse.sh
+++ systems/opensuse.sh
@@ -8,13 +8,16 @@ alias gpkg="zypper search -i"
 alias spkg="zypper search"
 
 updnow () {
+  sudo -v
   if command -v snap > /dev/null 2>&1; then
     sudo snap refresh
   fi
   if command -v flatpak > /dev/null 2>&1; then
     flatpak update -y
   fi
-  sudo zypper refresh && sudo zypper dup --no-confirm
+  sudo zypper refresh &&\
+  sudo zypper dup --download-only --no-confirm --force-resolution &&\
+  sudo zypper dup --force-resolution
 }
 
 ipkg () { sudo zypper install -y "./${1}"; }
