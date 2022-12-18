8b75b79c1cba067c876ed1da180dbeafbdf6968e
replicajune <replicajune@users.noreply.github.com>
2022-12-17 19:19:04 -0500

    2022 50 (#94)
    ✨ add:
    * light/b alias to increase or decrease brightness with ddcutil
    * functional tests for CI
    
    🔨 change:
    * in rare cases, USER variable may be undefined, but whoami should be there everytime
    
     👉 fix:
    * shellcheck is evaluated by busybox based shell, but <() is undefine in posix

--- shellconfig.sh
+++ shellconfig.sh
@@ -11,9 +11,9 @@ esac
 # I have to assume the location of this directory as ${_HOME}/.shellconfig to
 # help with needed modularity of files
 if [ "$(uname -s)" = "Darwin" ]; then
-  _HOME="/Users/${SUDO_USER-$USER}"
+  _HOME="/Users/${SUDO_USER-$(whoami)}"
 else
-  _HOME="$(getent passwd "${SUDO_USER-$USER}" | cut -d: -f6)"
+  _HOME="$(getent passwd "${SUDO_USER-$(whoami)}" | cut -d: -f6)"
 fi
 REPO_PATH="${_HOME}/.shellconfig"
 
@@ -198,94 +198,6 @@ alias oc=openstack
 alias tf=terraform
 
 # Kubernetes
-kset (){
-  # set completions and aliases for kubectl and helm "on demand"
-  # Need to stay as a function as it's sourcing stuff and creating aliases
-  # I bet kubectlx/kubens is much better but eh..
-
-  local USAGE
-  local KEY
-  local VALUE
-  local NAMESPACE
-  local CLUSTER
-  local COMPLETION
-  local ARGUMENTS
-
-  USAGE='
-  usage : kset [OPTIONS]
-    -c --cluster CLUSTER     : will source ~/.kubeconfig.CLUSTER.yml
-    -n --namespace NAMESPACE : set given namespace option to kubectl
-    -r --reset               : remove aliases
-    -x --completion          : if other arguements are given, will also set auto
-                              completion. If no other option are set, this
-                              option is implied
-  '
-
-  while [ ${#} -gt 0 ]; do
-    KEY="${1}"
-    VALUE="${2}"
-    case $KEY in
-      -n|--namespace)
-        NAMESPACE="${VALUE}"
-        shift # past argument
-      ;;
-      -c|--cluster)
-        CLUSTER="${VALUE}"
-        shift # past argument
-      ;;
-      -x|--completion)
-        COMPLETION='TRUE'
-      ;;
-      -r|--reset)
-        echo "removing aliases"
-        unalias k > /dev/null 2>&1 || true
-        unalias kubectl > /dev/null 2>&1 || true
-        return 0
-      ;;
-      -h|--help)
-        echo "${USAGE}" | grep -Ev '^$' | sed 's/^  //'
-        return 0
-      ;;
-      *)
-        echo "${USAGE}" | grep -Ev '^$' | sed 's/^  //'
-        return 0
-      ;;
-    esac
-    shift
-  done
-
-  if [ -n "${CLUSTER}" ]; then
-    ARGUMENTS="${ARGUMENTS} --kubeconfig ~/.kubeconfig.${CLUSTER}.yml"
-  fi
-
-  if [ -n "${NAMESPACE}" ]; then
-    ARGUMENTS="${ARGUMENTS} --namespace ${NAMESPACE}"
-  fi
-
-  if { [ -z "${CLUSTER}" ] && [ -z "${CLUSTER}" ]; } \
-  || [ "${COMPLETION}" = "TRUE" ]; then
-    if command -v kubectl > /dev/null 2>&1; then
-      echo "set kubectl completion for kubectl, k"
-      # shellcheck disable=SC3001
-      . <(kubectl completion bash)
-      # shellcheck disable=SC3001
-      . <(kubectl completion bash | sed 's/kubectl/k/g')
-      echo "set k alias"
-      alias k='kubectl'
-    fi
-    if command -v helm > /dev/null 2>&1; then
-      echo "set helm completion"
-      # shellcheck disable=SC3001
-      . <(helm completion bash)
-    fi
-  else
-    if [ -n "${ARGUMENTS}" ]; then
-      echo "define following arguments through kubectl alias:${ARGUMENTS}"
-      # shellcheck disable=SC2139
-      alias kubectl="kubectl ${ARGUMENTS}"
-    fi
-  fi
-}
 
 # git
 alias lgt=lazygit
--- shells/extras.sh
+++ shells/extras.sh
@@ -22,3 +22,90 @@ alias lsm="mount | grep -E '^(/dev|//)' | column -t"
 
 # shellcheck disable=SC2142
 alias ha="history | awk '{ print substr(\$0, index(\$0,\$4)) }' | sort | uniq -c | sort -h | grep -E '^[[:space:]]+[[:digit:]]+[[:space:]].{9,}$'"
+
+# Kubernetes
+  kset () {
+    # set completions and aliases for kubectl and helm "on demand"
+    # Need to stay as a function as it's sourcing stuff and creating aliases
+    # I bet kubectlx/kubens is much better but eh..
+
+    local USAGE
+    local KEY
+    local VALUE
+    local NAMESPACE
+    local CLUSTER
+    local COMPLETION
+    local ARGUMENTS
+
+    USAGE='
+    usage : kset [OPTIONS]
+      -c --cluster CLUSTER     : will source ~/.kubeconfig.CLUSTER.yml
+      -n --namespace NAMESPACE : set given namespace option to kubectl
+      -r --reset               : remove aliases
+      -x --completion          : if other arguements are given, will also set auto
+                                completion. If no other option are set, this
+                                option is implied
+    '
+
+    while [ ${#} -gt 0 ]; do
+      KEY="${1}"
+      VALUE="${2}"
+      case $KEY in
+        -n|--namespace)
+          NAMESPACE="${VALUE}"
+          shift # past argument
+        ;;
+        -c|--cluster)
+          CLUSTER="${VALUE}"
+          shift # past argument
+        ;;
+        -x|--completion)
+          COMPLETION='TRUE'
+        ;;
+        -r|--reset)
+          echo "removing aliases"
+          unalias k > /dev/null 2>&1 || true
+          unalias kubectl > /dev/null 2>&1 || true
+          return 0
+        ;;
+        -h|--help)
+          echo "${USAGE}" | grep -Ev '^$' | sed 's/^  //'
+          return 0
+        ;;
+        *)
+          echo "${USAGE}" | grep -Ev '^$' | sed 's/^  //'
+          return 0
+        ;;
+      esac
+      shift
+    done
+
+    if [ -n "${CLUSTER}" ]; then
+      ARGUMENTS="${ARGUMENTS} --kubeconfig ~/.kubeconfig.${CLUSTER}.yml"
+    fi
+
+    if [ -n "${NAMESPACE}" ]; then
+      ARGUMENTS="${ARGUMENTS} --namespace ${NAMESPACE}"
+    fi
+
+    if { [ -z "${CLUSTER}" ] && [ -z "${CLUSTER}" ]; } \
+    || [ "${COMPLETION}" = "TRUE" ]; then
+      if command -v kubectl > /dev/null 2>&1; then
+        echo "set kubectl completion for kubectl, k"
+        . <(kubectl completion bash)
+        . <(kubectl completion bash | sed 's/kubectl/k/g')
+        echo "set k alias"
+        alias k='kubectl'
+      fi
+      if command -v helm > /dev/null 2>&1; then
+        echo "set helm completion"
+        . <(helm completion bash)
+      fi
+    else
+      if [ -n "${ARGUMENTS}" ]; then
+        echo "define following arguments through kubectl alias:${ARGUMENTS}"
+        # shellcheck disable=SC2139
+        alias kubectl="kubectl ${ARGUMENTS}"
+      fi
+    fi
+  }
