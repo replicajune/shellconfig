a0c82c4a8b30dd61f3831a9a7736c0e615280bf4
replicajune <replicajune@users.noreply.github.com>
2023-01-13 17:24:51 -0500

    ✨ add: aspell aliases

--- systems/linux.sh
+++ systems/linux.sh
@@ -168,3 +168,10 @@ if command -v ddcutil > /dev/null 2>&1; then
   alias light='sudo ddcutil setvcp 10'
   alias b='light'
 fi
+
+if command -v aspell > /dev/null 2>&1; then
+  alias spellcheck='aspell check --master="${LANG%%\.*}" --lang="${LANG%%\.*}"  --sug-mode=slow -x --mode=markdown'
+  alias spell=spellcheck
+  alias spc='aspell check --master="${LANG%%\.*}" --lang="${LANG%%\.*}"  --sug-mode=slow -x --mode=markdown'
+fi
+
